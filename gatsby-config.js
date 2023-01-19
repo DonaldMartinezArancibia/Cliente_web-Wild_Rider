6f62c1150199e703500d349fbe7048e7b7987b2d
DonaldMartinezArancibia <antonio.arancibia.18@gmail.com>
2023-01-19 16:47:11 -0600

    Prueba Apollo-Client

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,12 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+ require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -39,5 +45,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-apollo',
+    
+
+    // {
+    //   resolve: `gatsby-source-strapi`,
+    //   options:{
+    //     apiURL: process.env.STRAPI_API_URL,
+    //     accessToken: process.env.STRAPI_TOKEN,
+    //     queryLimit: 25,
+    //     collectionTypes: ['post', 'email'],
+    //   },
+    // },
   ],
 }
