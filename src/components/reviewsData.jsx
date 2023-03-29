75493c662df484be91d993372b88c1d84d748a15
DonaldMartinezArancibia <antonio.arancibia.18@gmail.com>
2023-03-29 11:36:56 -0600

    Estilos de reseñas añadidos

--- src/components/reviewsData.jsx
+++ src/components/reviewsData.jsx
@@ -1,17 +1,12 @@
-import React, { Component } from "react"
+import React, { useState, useEffect } from "react"
+import { StarIcon } from "@heroicons/react/24/solid"
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react"
 
-class MapContainer extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      reviews: [],
-    }
-  }
+const MapContainer = props => {
+  const [reviews, setReviews] = useState([])
 
-  componentDidMount() {
-    const { google } = this.props
-    const service = new google.maps.places.PlacesService(
+  useEffect(() => {
+    const service = new props.google.maps.places.PlacesService(
       document.createElement("section")
     )
     const request = {
@@ -20,32 +15,81 @@ class MapContainer extends Component {
     }
 
     service.getDetails(request, (place, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        this.setState({
-          reviews: place.reviews,
-        })
+      if (status === props.google.maps.places.PlacesServiceStatus.OK) {
+        setReviews(place.reviews)
       }
     })
+  }, [props.google.maps.places.PlacesServiceStatus.OK])
+
+  const [expandedReviews, setExpandedReviews] = useState([])
+
+  const handleExpandClick = index => {
+    const newExpandedReviews = [...expandedReviews]
+    newExpandedReviews[index] = !expandedReviews[index]
+    setExpandedReviews(newExpandedReviews)
   }
 
-  render() {
-    const { reviews } = this.state
-
-    return (
-      <div>
-        <h1>Reviews:</h1>
+  return (
+    <div className="py-8 mx-16">
+      <h1 className="mb-4 text-3xl font-bold">Reviews</h1>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
         {reviews.map((review, index) => (
-          <div key={index}>
-            <p>Author: {review.author_name}</p>
-            <p>Rating: {review.rating}</p>
-            <p>Text: {review.text}</p>
+          <div
+            key={index}
+            className="flex p-4 border border-gray-300 rounded-md"
+          >
+            <img
+              src={review.profile_photo_url}
+              alt="Profile"
+              className="w-12 h-12 mr-4 rounded-full"
+            />
+            <div>
+              <h2 className="mb-2 text-lg font-bold">{review.author_name}</h2>
+              <div className="flex items-center mb-2">
+                {Array.from(Array(5), (_, i) => (
+                  <StarIcon
+                    key={i}
+                    className={`h-4 w-4 ${
+                      review.rating >= i + 1
+                        ? "text-yellow-500"
+                        : "text-gray-400"
+                    }`}
+                  />
+                ))}
+              </div>
+              {review.text.length > 150 ? (
+                <>
+                  <p className="text-gray-600">
+                    {expandedReviews[index]
+                      ? review.text
+                      : `${review.text.slice(0, 150)}... `}
+                    <a
+                      href="#"
+                      onClick={e => {
+                        e.preventDefault()
+                        handleExpandClick(index)
+                      }}
+                      className="text-blue-500"
+                    >
+                      {expandedReviews[index] ? " Leer menos" : " Leer más"}
+                    </a>
+                  </p>
+                </>
+              ) : (
+                <p className="text-gray-600">{review.text}</p>
+              )}
+              <p className="text-xs text-gray-400">
+                {review.relative_time_description}
+              </p>
+            </div>
           </div>
         ))}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default GoogleApiWrapper({
   apiKey: "AIzaSyB2H-WpOFQN0yyp8ARh4sl36uWL_Mb0ALE",
+  language: "en",
 })(MapContainer)
