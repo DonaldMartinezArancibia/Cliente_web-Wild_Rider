e4afa88d6758190b19b631686c6abefb7ec1d69b
DonaldMartinezArancibia <antonio.arancibia.18@gmail.com>
2023-10-24 14:15:23 -0600

    Formulario de quote ajustado para ser registrado en netlify

---
+++ src/components/carQuoteQuery.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { useQuery } from "@apollo/client"
+import { Cars } from "../gql/carsByIdQuery"
+import CarFormHtml from "./carQuoteForm"
+
+const CarQuoteQuery = pageContext => {
+  const { carName, remoteId } = pageContext.pageContext
+  //   console.log(remoteId)
+  const {
+    data: carsById,
+    loading: carsByIdQueryLoading,
+    error: carsByIdQueryError,
+  } = useQuery(Cars, {
+    variables: { internalId: remoteId, locale: ["en"] },
+  })
+  if (carsByIdQueryLoading) return <p>Loading...</p>
+  if (carsByIdQueryError) return <p>Error : {carsByIdQueryError.message}</p>
+
+  return <div>{carsById && <CarFormHtml apolloData={carsById} />}</div>
+}
+export default CarQuoteQuery
